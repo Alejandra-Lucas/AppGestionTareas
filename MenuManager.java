686b56c86aecc2663806aaf54990e70c4d79b1c3
Alejandra Lucas <lucasalejandra217@gmail.com>
2025-11-23 23:11:33 -0600

    [Alejandra-Lucas] Agrego menús por tipo de usuario y métodos necesarios

--- MenuManager.java
+++ MenuManager.java
@@ -9,6 +9,7 @@ public class MenuManager {
     private GestorUsuarios gestorUsuarios;
     private GestorTareas gestorTareas;
     private Usuario usuarioActual;
+
     public MenuManager() {
         this.scanner = new Scanner(System.in);
         this.sistemaLogin = new SistemaLogin();
@@ -16,7 +17,70 @@ public class MenuManager {
         this.gestorTareas = new GestorTareas();
     }
 
+    // Definimos métodos necesarios para el menú
+
+    private int leerEntero() {
+        // Método para leer números enteros
+        try{
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            return -1; // Valor inválido
+        }
+    }
+
+    private LocalDate leerFecha(String mensaje) {
+        // Método para leer fechas en formato AAAA-MM-DD
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        while (true) {
+            System.out.print(mensaje);
+            String entrada = scanner.nextLine();
+            try {
+                return LocalDate.parse(entrada, formatter);
+            } catch (DateTimeParseException e) {
+                System.out.println("Formato de fecha inválido. Por favor, use AAAA-MM-DD.");
+            }
+        }
+    }
+
+    private void crearTarea(boolean esAdmin){
+        // Método para crear una nueva tarea
+        try{
+            System.out.print("Crear nueva tarea: ");
+            System.out.print("Ingrese una descripción de la tarea: ");
+            String descripcion = scanner.nextLine();
+
+            Usuario asignado = usuarioActual;
+            if (esAdmin){
+                System.out.println("Ingrese el nickname del usuario asignado: ");
+                System.out.print("Si desea asignarse a sí mismo, deje en blanco y presione Enter ");
+                String nickname = scanner.nextLine();
+                if (!nickname.isEmpty()){
+                    asignado = gestorUsuarios.getUsuarioPorNickname(nickname);
+                    if (asignado == null){
+                        System.out.println("Usuario no encontrado.");
+                        return;
+                    }
+                }
+            }
+            // Validar fecha
+            LocalDate fechaInicioEst = leerFecha("Fecha estimada de inicio (AAAA-MM-DD: ");
+            LocalDate fechaFinEst = leerFecha("Fecha estimada de fin (AAAA-MM-DD): ");
+
+            if (fechaFinEst.isBefore(fechaInicioEst)) {
+                throw new IllegalArgumentException("La fecha de finalización no puede ser antes que la fecha de inicio.")
+                return;
+            }
+            Tarea nuevaTarea = new Tarea(descripcion, asignado, fechaInicioEst, fechaFinEst);
+        } catch (Exception e){
+            System.out.println("Error al crear la tarea: " + e.getMessage());
+        }
+    }
+
+    /*
+    Métodos para registrar, actualizar y eliminar tareas */
+
     public void inicio(){
+        // Método principal para iniciar el menú
         boolean salir = false;
         while (!salir) {
             try{
@@ -46,6 +110,8 @@ public class MenuManager {
     } // Fin método inicio
 
     private void procesarLogin() {
+
+        // Método para procesar el inicio de sesión
         System.out.print("Ingrese su correo o nickname: ");
         String credencial = scanner.nextLine();
         System.out.print("Ingrese su contraseña: ");
@@ -59,6 +125,8 @@ public class MenuManager {
     }
 
     private void mostrarMenuCorrespondiente() {
+
+        // Método para mostrar el menú según el tipo de usuario
         if (usuarioActual instanceof Administrador) {
             menuAdmin();
         } else if (usuarioActual instanceof Desarrollador) {
@@ -81,25 +149,79 @@ public class MenuManager {
             System.out.println("4. Actualizar tarea");
             System.out.println("5. Eliminar tarea");
             System.out.println("6. Cerrar sesión");
+
             System.out.print("Seleccione una opción: ");
             int opcion = leerEntero();
+            
+            switch (opcion){
+                case 1: 
+                    registrarUsuario();
+                    break;
+                case 2: 
+                    crearTarea(true);
+                    break;
+                case 3:
+                    gestorTareas.getTodasLasTareas();
+                    break;
+                case 4:
+                    actualizarTarea(true);
+                    break;
+                case 5:
+                    eliminarTarea();
+                    break;
+                case 6:
+                    salir = true;
+                    usuarioActual = null;
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+                    break;
+            }
         }
     }
 
     // ------ Menú Desarrollador ------
-    private void menuDesarrollador(){}
+    private void menuDesarrollador(){
+        boolean salir = false;
+        while (!salir) {
+            System.out.println("--- Menú Desarrollador ---");
+            System.out.println("1. Crear tareas");
+            System.out.println("2. Ver mis tareas");
+            System.out.println("3. Actualizar tareas");
+            System.out.println("4. Cerrar sesión");
 
-    // ------ Menú Invitado ------
-    private void menuInvitado(){}
-
-    // Método para leer números enteros
-    private int leerEntero() {
-        try{
-            return Integer.parseInt(scanner.nextLine());
-        } catch (NumberFormatException e) {
-            return -1; // Valor inválido
+            System.out.print("Seleccione una opción: ");
+            int opcion = leerEntero();
+            
+            switch (opcion){
+                case 1: 
+                    crearTarea(false);
+                    break;
+                case 2:
+                    gestorTareas.getTareasPorUsuario(usuarioActual.getNickname());
+                    break;
+                case 3:
+                    actualizarTarea(false);
+                    break;
+                case 4:
+                    salir = true;
+                    usuarioActual = null;
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+                    break;
+            }
         }
     }
 
+    // ------ Menú Invitado ------
+    private void menuInvitado(){
+        System.out.println("---Menú Invitado ---");
+        System.out.println("Tareas asignadas: ");
+        gestorTareas.getTareasPorUsuario(usuarioActual.getNickname());
+        System.out.println("Presione Enter para cerrar sesión.");
+        scanner.nextLine();
+        usuarioActual = null;
+    }
 
 } // Fin de la clase MenuManager
