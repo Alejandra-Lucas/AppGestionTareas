624a0f35d1e7a01c33c35b6739b42f382619a0f9
rodrineitor3000 <rodrigo1303@ciencias.unam.mx>
2025-12-03 09:14:12 -0600

    Update Persistencia.java

--- Persistencia.java
+++ Persistencia.java
@@ -1,4 +1,4 @@
-import java.io.*;
+mport java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -71,4 +71,3 @@ public class Persistencia {
         return new ArrayList<>();
     }
 }
-
